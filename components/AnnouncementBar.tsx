a47ee85862751c1ea034a7204e20de90159cec18
AlbertoRodBa <rodriguez.barrios.alberto@gmail.com>
2026-05-20 13:32:17 -0400

    cambio en hero title

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AnnouncementBar() {
   const items = [
-    "⭐ Sitios web modernos y optimizados",
+    "Sitios web modernos y optimizados",
     "Entrega rápida",
     "Respuesta en menos de 24 horas",
   ];
