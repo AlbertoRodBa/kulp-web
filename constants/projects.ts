5b486ecd238c218c90c1d54806f3650f733d8032
AlbertoRodBa <rodriguez.barrios.alberto@gmail.com>
2026-06-12 18:47:48 -0400

    edicion en descripcion de proyectos mas corta para mobile v2

--- constants/projects.ts
+++ constants/projects.ts
@@ -6,7 +6,7 @@ export const projects = [
     year: "2026",
     link: "https://pladecocerrillos.participayplanifica.cl",
     description:
-      "Página web para la gestión del Plan de Desarrollo Comunal (PLADECO) de Cerrillos.",
+      "Página web para la gestión del Plan de Desarrollo Comunal (PLADECO) de Cerrillos. El sitio está pensado para la publicación de información actualizada del proceso.",
     image: "/project-1-pladeco-cerrillos.jpg",
     tags: ["Participación Ciudadana", "Desarrollo Local"],
   },
@@ -28,7 +28,7 @@ export const projects = [
     year: "2025",
     link: "https://piimep.penaflor.cl",
     description:
-      "Sitio web para informar sobre el PIIMEP de la comuna de Peñaflor y sus actividades de participación.",
+      "Sitio web para informar sobre el PIIMEP de la comuna de Peñaflor, llamados a actividades de participación y consultas ciudadanas.",
     image: "/project-3-piimep-penaflor.jpg",
     tags: ["Planificación Urbana", "Participación Ciudadana"],
   },
