f109c90556b90994aeae4652b266c3dbdc532b72
AlbertoRodBa <rodriguez.barrios.alberto@gmail.com>
2026-08-14 12:37:39 -0400

    Cambiar textos en barra de anuncios

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AnnouncementBar() {
   const items = [
     "💫 Sitios web diseñados a medida",
-    "Adaptación a todos los dispositivos",
+    "Diseño adaptado a todos los dispositivos",
     "Respuesta en menos de 24 horas",
   ];
 
